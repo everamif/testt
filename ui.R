4b00ca6b38a68a6796d70f513cab747639be442e
everamif <71084877+everamif@users.noreply.github.com>
2022-05-19 16:34:44 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,3 @@
-library(shiny)
-library(DT)
-
-this_table = data.frame(bins = c(30, 50), cb = c(T, F))
-
 ui <- fluidPage(
   
   sidebarLayout(
@@ -22,29 +17,3 @@ ui <- fluidPage(
     )
   )
 )
-
-server <- function(input, output) {
-  
-  this_table <- reactiveVal(this_table)
-  
-  observeEvent(input$add_btn, {
-    t = rbind(data.frame(bins = input$bins,
-                         cb = input$cb), this_table())
-    this_table(t)
-  })
-  
-  observeEvent(input$delete_btn, {
-    t = this_table()
-    print(nrow(t))
-    if (!is.null(input$shiny_table_rows_selected)) {
-      t <- t[-as.numeric(input$shiny_table_rows_selected),]
-    }
-    this_table(t)
-  })
-  
-  output$shiny_table <- renderDT({
-    datatable(this_table(), selection = 'single', options = list(dom = 't'))
-  })
-}
-
-shinyApp(ui = ui, server = server)
